2b10408501e6a2e233f1710919fc805b12cce28b
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-01-17 16:23:25 +0530

    Added domain name in allowed hosts

--- LeavePortal/settings.py
+++ LeavePortal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.234.29.48', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['13.234.29.48', 'localhost', '127.0.0.1', 'leaveportal.cinemato.site']
 
 
 # Application definition
