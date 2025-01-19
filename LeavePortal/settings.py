84ac9a824db329619f5600de01ccc4895c8b1630
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-01-19 14:10:28 +0530

    added vercel url path in CORS_ALLOWED_ORIGINS and CSRF_TRUSTED_ORIGINS = [

--- LeavePortal/settings.py
+++ LeavePortal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.234.29.48', 'localhost', '127.0.0.1', 'leaveportal.cinemato.site']
+ALLOWED_HOSTS = ['13.234.29.48', 'localhost', '127.0.0.1', 'www.leaveportal.cinemato.site', 'leaveportal.cinemato.site']
 
 
 # Application definition
@@ -79,10 +79,12 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://leave-portal-mauve.vercel.app/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+    "https://leave-portal-mauve.vercel.app/",
 ]
 
 MIDDLEWARE = [
